db448e1a03793b2422c43de2de0c7fe2b09ee13e
cjohnson <collin.g.johnson@gmail.com>
2023-06-28 22:53:42 -0400

    Add MDASTTextNode

---
+++ MDASTDotNet/LeafBlocks/MDASTTextNode.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace MDASTDotNet.LeafBlocks
+{
+	[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+	public class MDASTTextNode : MDASTNode
+	{
+		[JsonProperty("content")]
+		public string? Content { get; set; }
+
+		public MDASTTextNode(string? content) : base("text")
+		{
+			Content = content;
+		}
+	}
+}
