3b3baf21bda5e10796c0f7948bf4b419dc7fb09d
cjohnson <collin.g.johnson@gmail.com>
2023-07-06 21:19:36 -0400

    Make test name more verbose, add source comments to CommonMark spec

--- MDASTDotNet.Test/MDASTThematicBreakTests.cs
+++ MDASTDotNet.Test/MDASTThematicBreakTests.cs
@@ -6,8 +6,12 @@ namespace MDASTDotNet.Test;
 [TestClass]
 public class MDASTThematicBreakTests
 {
+	/// <summary>
+	/// <see href="https://spec.commonmark.org/0.30/">CommonMark 0.30</see>: Implementation of
+	/// <see href="https://spec.commonmark.org/0.30/#example-43">Thematic Break Example 43</see>
+	/// </summary>
 	[TestMethod]
-	public void BasicUsageTests()
+	public void BasicDeclarationOfThematicBreak()
 	{
 		var parser = new MDASTParser();
 
