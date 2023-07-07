a8159d43515ec249da17330b52de690b942b4a75
cjohnson <collin.g.johnson@gmail.com>
2023-07-06 21:30:19 -0400

    Add test for CommonMark 0.30 Example 44 & 45

--- MDASTDotNet.Test/MDASTThematicBreakTests.cs
+++ MDASTDotNet.Test/MDASTThematicBreakTests.cs
@@ -31,4 +31,25 @@ public class MDASTThematicBreakTests
 		
 		Assert.AreEqual(expected, actual);
 	}
+
+	/// <summary>
+	/// <see href="https://spec.commonmark.org/0.30/">CommonMark 0.30</see>: Implementation of
+	/// <see href="https://spec.commonmark.org/0.30/#example-44">Thematic Break Example 44</see>
+	/// and
+	/// <see href="https://spec.commonmark.org/0.30/#example-45">Thematic Break Example 45</see>
+	/// </summary>
+	[DataRow("+++")]
+	[DataRow("===")]
+	[TestMethod]
+	public void WrongCharacterDeclarationOfThematicBreak(string markdownInput)
+	{
+		var parser = new MDASTParser();
+
+		var actual = parser.Parse(markdownInput);
+
+		var expected = new MDASTRootNode();
+		expected.Children.Add(new MDASTTextNode(markdownInput));
+
+		Assert.AreEqual(expected, actual);
+	}
 }
