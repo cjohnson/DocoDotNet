663672a724e15d106f2b99185b036a7b34e49a30
cjohnson <collin.g.johnson@gmail.com>
2023-07-22 01:42:47 -0400

    Add polishing to HeadingNodeParser

--- MDASTDotNet/Parser/HeadingNodeParser.cs
+++ MDASTDotNet/Parser/HeadingNodeParser.cs
@@ -13,7 +13,7 @@ internal partial class HeadingNodeParser : IParser<HeadingNode>
 	/// <see href="https://spec.commonmark.org/0.30/">CommonMark 0.30 Specification.</see>
 	/// </summary>
 	[GeneratedRegex(@"^(?:(?: {0,3}|\t{0,3})(#{1,6})(?: +|\t+|$))(?:([^#\n].*?)(?: +|\t+|$))?(?:#*)?(?: *|\t*)$", RegexOptions.Multiline)]
-	internal static partial Regex HeadingRegex();
+	internal static partial Regex ATXHeadingRegex();
 
 	/// <summary>
 	/// Attempts to parse an <see href="https://spec.commonmark.org/0.30/#atx-headings">ATX Heading</see> from the given string line 
@@ -23,7 +23,7 @@ internal partial class HeadingNodeParser : IParser<HeadingNode>
 	/// <returns>An MDASTHeadingNode on success, and null on failure.</returns>
 	public HeadingNode? Parse(string target)
 	{
-		var headingRegex = HeadingRegex();
+		var headingRegex = ATXHeadingRegex();
 
 		var match = headingRegex.Match(target);
 		if (!match.Success)
@@ -31,6 +31,8 @@ internal partial class HeadingNodeParser : IParser<HeadingNode>
 			return null;
 		}
 
+		// The first capturing group (2nd group) in the ATX Heading regex is the header hashtags substring,
+		// whose length corresponds to the header level. (For example, '##' -> Level = 2)
 		var level = match.Groups[1].Value.Length;
 		var text = new MDASTTextNode(match.Groups[2].Value);
 
