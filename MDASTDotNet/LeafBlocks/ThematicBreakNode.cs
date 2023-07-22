94f911c717e2ef8332674f7eb262ecff5905d3c8
cjohnson <collin.g.johnson@gmail.com>
2023-07-22 02:50:35 -0400

    Add more documentation comments to ThematicBreakNode

--- MDASTDotNet/LeafBlocks/ThematicBreakNode.cs
+++ MDASTDotNet/LeafBlocks/ThematicBreakNode.cs
@@ -5,7 +5,12 @@ using MDASTDotNet.Extensions;
 namespace MDASTDotNet.LeafBlocks;
 
 /// <summary>
-/// A Thematic Break node represents a thematic break (traditionally a &lt;br/&gt; tag in HTML).
+/// A Thematic Break node represents a thematic break (traditionally a &lt;br/&gt; tag in HTML)
+/// <br/><br/>
+/// According to the <see href="https://spec.commonmark.org/0.30/">CommonMark 0.30 Specification</see>:
+/// <br/>
+/// A line consisting of optionally up to three spaces of indentation, followed by a sequence of three or more matching -, _, or * characters, 
+/// each followed optionally by any number of spaces or tabs, forms a <see href="https://spec.commonmark.org/0.30/#thematic-breaks">Thematic Break</see>.
 /// </summary>
 [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
 public class ThematicBreakNode : INode
